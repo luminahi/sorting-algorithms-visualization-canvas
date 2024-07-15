b9b7b74b65c48e4d4416d6e52e1484a220440504
Alex Lima <delimaferreiraalex@gmail.com>
2024-07-14 23:25:26 -0300

    ref: add new ways to generate bars

--- src/ItemManager.ts
+++ src/ItemManager.ts
@@ -1,7 +1,7 @@
 import { Item } from "./Item.js";
 
 export class ItemManager {
-  private items: Item[];
+  public items: Item[];
   private screenWidth: number;
   private screenHeight: number;
   private arraySize: number;
@@ -21,13 +21,19 @@ export class ItemManager {
     this.isRunning = false;
   }
 
-  public printToScreen() {
+  public paintBackground() {
     this.ctx.fillStyle = `rgba(0, 0, 0, 1)`;
     this.ctx.fillRect(0, 0, this.screenWidth, this.screenHeight);
+  }
+
+  public paintItems() {
+    this.paintBackground();
     for (const item of this.items) item.draw();
   }
 
-  public generateItems() {
+  public generateItemsRandom() {
+    if (this.isRunning) this.stopAnimation();
+
     this.items = new Array(this.arraySize);
 
     for (let i = 0; i < this.arraySize; i++) {
@@ -44,12 +50,53 @@ export class ItemManager {
     }
   }
 
+  public generateItemDescending() {
+    if (this.isRunning) this.stopAnimation();
+
+    this.items = new Array(this.arraySize);
+
+    let max = this.arraySize * 10 - 5;
+    for (let i = 0; i < this.arraySize; i++) {
+      const value = max;
+      max -= 10;
+
+      this.items[i] = new Item(
+        15 * i + 2,
+        this.screenHeight,
+        10,
+        value,
+        this.ctx,
+        "white"
+      );
+    }
+  }
+
+  public generateItemAscending() {
+    if (this.isRunning) this.stopAnimation();
+
+    this.items = new Array(this.arraySize);
+
+    for (let i = 0; i < this.arraySize; i++) {
+      const value = i * 10 + 5;
+
+      this.items[i] = new Item(
+        15 * i + 2,
+        this.screenHeight,
+        10,
+        value,
+        this.ctx,
+        "white"
+      );
+    }
+  }
+
   public runAnimation(sortingAlgorithm: Function, animationSpeed: number) {
     this.isRunning = true;
+
     sortingAlgorithm(
       this.items,
       () => {
-        this.printToScreen();
+        this.paintItems();
         return this.isRunning;
       },
       animationSpeed
