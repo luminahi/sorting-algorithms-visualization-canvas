edcfee33d8c2da6d5db9264982e799aba5d1a27d
Alex Lima <delimaferreiraalex@gmail.com>
2024-07-16 20:21:35 -0300

    feat: add heap sort animation

---
+++ src/sorting/heap_sort.ts
@@ -0,0 +1,119 @@
+import { Item } from "../Item.js";
+import { delay, swap } from "../util.js";
+
+class MinHeap {
+  private array: number[];
+  private size: number;
+  private maxSize: number;
+
+  public constructor(maxSize: number) {
+    this.array = new Array(maxSize);
+    this.maxSize = maxSize;
+    this.size = 0;
+  }
+
+  public insert(item: number) {
+    if (this.size >= this.maxSize) {
+      console.log("heap is full");
+      return;
+    }
+
+    this.size++;
+    this.array[this.size - 1] = item;
+    this.heapifyUp(this.size - 1);
+  }
+
+  public extractMin() {
+    if (this.size <= 0) {
+      console.log("heap is empty");
+      return -1;
+    }
+
+    let value = this.array[0];
+
+    this.array[0] = this.array[this.size - 1];
+    this.array[this.size - 1] = 0;
+    this.size--;
+
+    this.heapifyDown(0);
+
+    return value;
+  }
+
+  private heapifyDown(index: number) {
+    let smallest = index;
+    let left = 2 * index + 1;
+    let right = 2 * index + 2;
+
+    if (left < this.size && this.array[left] < this.array[smallest])
+      smallest = left;
+
+    if (right < this.size && this.array[right] < this.array[smallest])
+      smallest = right;
+
+    if (smallest != index) {
+      this.swap(smallest, index);
+      this.heapifyDown(smallest);
+    }
+  }
+
+  private heapifyUp(index: number) {
+    let parent: number = ((index - 1) / 2) | 0;
+
+    if (parent >= 0 && this.array[index] < this.array[parent]) {
+      this.swap(index, parent);
+      this.heapifyUp(parent);
+    }
+  }
+
+  private swap(a: number, b: number) {
+    if (!this.array[a] || !this.array[b]) return;
+
+    let aux = this.array[a];
+    this.array[a] = this.array[b];
+    this.array[b] = aux;
+  }
+
+  public getSize() {
+    return this.size;
+  }
+
+  public getArray() {
+    return this.array;
+  }
+
+  public async sort(array: Item[], fn: Function, animationSpeed: number) {
+    const length = array.length;
+
+    for (let i = 0; i < length; i++) {
+      array[i].setValue(this.extractMin());
+
+      array[i].setColor("red");
+      fn(array);
+      await delay(animationSpeed);
+
+      array[i].setColor("white");
+    }
+
+    fn(array);
+    return array;
+  }
+}
+
+async function heapSort(array: Item[], fn: Function, animationSpeed: number) {
+  const heap = new MinHeap(array.length);
+
+  await new Promise(async (resolve, _reject) => {
+    for (let i = 0; i < array.length; i++) {
+      heap.insert(array[i].getValue());
+      array[i].setColor("blue");
+      fn(array);
+      await delay(animationSpeed);
+    }
+    resolve("");
+  });
+
+  heap.sort(array, fn, animationSpeed);
+}
+
+export { heapSort };
